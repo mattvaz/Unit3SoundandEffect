2d15b9f2ca521d24cf3bdb15617e0dd55e18b7af
mattvaz <146034846+mattvaz@users.noreply.github.com>
2024-04-29 15:21:36 -0700

    Addedd
    almost doneeeee.
    
    :D

--- SoundandEffectVazquezp8/Assets/Scripts/MoveLeft.cs
+++ SoundandEffectVazquezp8/Assets/Scripts/MoveLeft.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private PlayerController playerControllerScript;
     private float speed = 30;
+    private float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript =
+        GameObject.Find("player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      if (playerControllerScript.gameOver == false) 
         transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+            Destroy(gameObject);
     }
 }
--- SoundandEffectVazquezp8/Assets/Scripts/SpawnMangager.cs
+++ SoundandEffectVazquezp8/Assets/Scripts/SpawnMangager.cs
@@ -8,10 +8,13 @@ public class SpawnMangager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class SpawnMangager : MonoBehaviour
 
     void SpawnObstacle ()
     {
+        if((playerControllerScript.gameOver == false))
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
